36d7120ecfabfb62552670e804f9c555a2253976
saber son <elmohandesstamera@gmail.com>
2024-08-17 13:14:37 +0300

    Refactor

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tarweej_platform/tarweej_platform_app.dart';
 
-import 'package:tarweej_platform/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
